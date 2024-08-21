1819da4160c61580928d9f6003fa0516f2776f3a
kaoyuching <doris830426@gmail.com>
2024-08-21 15:01:24 +0800

    CHANGE: use torch operator in EffdetPostProcess

--- effdet_convert/post_process.py
+++ effdet_convert/post_process.py
@@ -27,8 +27,8 @@ class EffdetPostProcess(nn.Module):
             for level in range(self.num_levels)], 1)
 
         _, cls_topk_indices_all = torch.topk(cls_outputs_all.reshape(batch_size, -1), dim=1, k=self.max_detection_points)
-        indices_all = cls_topk_indices_all // self.num_classes
-        classes_all = cls_topk_indices_all % self.num_classes
+        indices_all = torch.div(cls_topk_indices_all, self.num_classes, rounding_mode='floor')
+        classes_all = torch.remainder(cls_topk_indices_all, self.num_classes)
 
         box_outputs_all_after_topk = torch.gather(
             box_outputs_all, 1, indices_all.unsqueeze(2).expand(-1, -1, 4))
